a98c54b2b5e7038bd33f504b30da7a0f38685335
RainbowFromHell <matveyrfh@mail.ru>
2021-12-12 17:37:00 +0300

    Add full project.

---
+++ Second_task/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Second_task;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        try
+        {
+            File.ReadAllLines("Test.txt").Where(x => x[0] == x[^1]).ToList().ForEach(x => Console.WriteLine(x));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+    }
+}
